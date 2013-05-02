f0b2292007b4583b3909139add4d0718c8022895
cattaka <cattaka@ctkx220.home.cattaka.net>
2013-05-03 04:17:01 +0900

    Modify UltimateTank.ino for NFC Shield. It needs D10-D13. So use other pins to avoid pins conflict.

---
+++ UltimateTank/src/net/cattaka/android/ultimatetank/util/CommandAdapterUtil.java
@@ -0,0 +1,26 @@
+
+package net.cattaka.android.ultimatetank.util;
+
+import net.cattaka.android.ultimatetank.usb.ICommandAdapter;
+
+public class CommandAdapterUtil {
+    /**
+     * @param adapter
+     * @param forward Value for forward. This must be between -1 and 1. When
+     *            value is positive it moves forward, when value is negative it
+     *            moves backward.
+     * @param turn Value for turn. This must be between -1 and 1. When value is
+     *            positive it turns right, when value is negative it turns left.
+     */
+    public static void sendMove(ICommandAdapter adapter, float forward, float turn) {
+        float leftMotor = Math.min(1, 1 + turn * 2) * forward;
+        float rightMotor = Math.min(1, 1 - turn * 2) * forward;
+
+        float leftMotor1 = (leftMotor > 0) ? leftMotor : 0;
+        float leftMotor2 = (leftMotor < 0) ? -leftMotor : 0;
+        float rightMotor1 = (rightMotor > 0) ? rightMotor : 0;
+        float rightMotor2 = (rightMotor < 0) ? -rightMotor : 0;
+
+        adapter.sendMove(leftMotor1, leftMotor2, rightMotor1, rightMotor2);
+    }
+}
